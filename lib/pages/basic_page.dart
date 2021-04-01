36c55e727b419e810b147212410c3be24d667ec9
PavelKlimes <klimes.pavel@protonmail.com>
2021-04-01 16:43:50 +0200

    Docume/*

--- lib/pages/basic_page.dart
+++ lib/pages/basic_page.dart
@@ -1,10 +1,24 @@
-import 'package:flutter/material.dart';
+/*
+TODO: 1. Vytvoříš si nové okno.
+TODO: 2. Zadáš stless nebo stful(podle potřeby).
+TODO: 3. Zkopíruješ to co je označeno -----.
+TODO: 4. A můžeš tvořit to co chceš v oknu.
+*/
 
 /*
+-------------------------------------------------------
 
+import 'package:flutter/material.dart';
 
+Scaffold(
+  appBar: AppBar(
+    //TODO: sem se přidává co chceš v appbaru
+  ),
+  body: //TODO: sem se přídáva co chceš v body
+  ,
+);
 
-
+-------------------------------------------------------
 
 
  */
