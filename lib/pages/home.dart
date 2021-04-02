24fbb4b86cd722519113f3c1b729f5a43501a018
henryklimes <henryklimes@protonmail.com>
2021-04-02 11:39:31 +0200

    editing home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,8 +11,12 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        backgroundColor: Colors.blue,
-        title: Text("Klimes Systems"),
+        backgroundColor: Colors.limeAccent.shade200,
+        title: Text(
+          "Distance meter",
+          style: TextStyle(color: Colors.grey.shade800,),
+        ),
+        centerTitle: true,
       ),
       body: Column(
 
