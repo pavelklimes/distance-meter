fd5e2162a6e051d2c5a8510035b822ed27f21cb5
PavelKlimes <klimes.pavel@protonmail.com>
2021-04-02 11:06:08 +0200

    New images

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'pages/loading_screen.dart';
 import 'pages/home.dart';
 import 'pages/active_walking.dart';
+import 'pages/end_walking.dart';
 
 
 void main() async => runApp(MaterialApp(
