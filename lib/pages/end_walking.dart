10ee4a9bef80e4f2dc68e706172b2a56cca056a2
PavelKlimes <klimes.pavel@protonmail.com>
2021-04-02 21:57:00 +0200

    Time on end_walking.dart

--- lib/pages/end_walking.dart
+++ lib/pages/end_walking.dart
@@ -15,11 +15,30 @@ class End_Walking extends StatelessWidget {
       body: Column(
         children: <Widget>[
 
-          // TODO: Využij proměnnou ourTime a vypiš dobu trvání
+
+          SizedBox(height: 160,),
+          Align(
+            alignment: Alignment(-0.51, 0.0),
+            child: Text(
+              "Doba trvání:",
+              style: TextStyle(
+                color: Colors.grey[850],
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Text(
+            ourTime,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 47.3,
+
+            ),
+          ),
 
 
-
-          SizedBox(height: 490.0,), //TODO: SizedBox upravit podle potřeby a po přidání widgetů.
+          SizedBox(height: 300.0,), //TODO: SizedBox upravit podle potřeby a po přidání widgetů.
           Align(
             alignment: Alignment(-0.0, 0.0),
             child: Image.asset(
