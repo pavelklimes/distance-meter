3bd67878db9b9a647cb0396bf78f267c9e05608c
henryklimes <henryklimes@protonmail.com>
2021-04-17 15:54:56 +0200

    editing delays

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,7 @@ import 'settings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-int delayTime = 30;
+int delayTime = 16;
 
 class Home extends StatefulWidget {
   @override
